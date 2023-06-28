b1c34a4a0c9a4812051824e8dd7fdfaa3a728244
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-06-28 18:13:03 +0200

    Tests on login.component : test submission of the form

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,10 +11,16 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let sessionService: SessionService;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -34,6 +40,10 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    sessionService = TestBed.inject(SessionService);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
@@ -62,5 +72,50 @@ describe('LoginComponent', () => {
       password: 'password123',
     });
     expect(component.form.valid).toBeTruthy();
+  });
+
+  it('should call the submit method when the form is submitted', () => {
+    const submitSpy = jest.spyOn(component, 'submit'); // Create a spy on the submit method
+
+    // simulate the form being submitted by triggering the submit event on the form
+    const form = fixture.nativeElement.querySelector('form'); // Get the DOM element representing the form
+    form.dispatchEvent(new Event('submit')); // trigger the submit event by creating a new instance of the event and dispatching it on the form
+
+    expect(submitSpy).toHaveBeenCalled(); // Check that the submit method has been called
+  });
+
+  it('should call login method of authService with correct login request', () => {
+    const loginSpy = jest.spyOn(authService, 'login').mockReturnValue(of()); // Crée un espion sur la méthode 'login' du service 'authService' et retourne un observable vide
+    const loginRequest = { email: 'test@example.com', password: 'password' };
+
+    component.form.setValue(loginRequest); // Définit les valeurs du formulaire
+
+    component.submit(); // Appelle la méthode 'submit'
+
+    expect(loginSpy).toHaveBeenCalledWith(loginRequest); // Vérifie si la méthode 'login' a été appelée avec les bonnes valeurs
+  });
+
+  it('should call logIn method of sessionService with correct session information', () => {
+    const sessionInformation = {
+      token: 'mockToken',
+      type: 'mockType',
+      id: 1,
+      username: 'mockUsername',
+      firstName: 'mockFirstName',
+      lastName: 'mockLastName',
+      admin: true,
+    };
+    jest.spyOn(authService, 'login').mockReturnValue(of(sessionInformation)); // Crée un espion sur la méthode 'login' du service 'authService' et retourne un observable contenant les informations de session
+    const sessionLoginSpy = jest.spyOn(sessionService, 'logIn'); // Crée un espion sur la méthode 'logIn' du service 'sessionService'
+    const loginRequest = {
+      email: 'mockUsername@example.com',
+      password: 'password',
+    }; // Crée une requête de connexion
+
+    component.form.setValue(loginRequest); // Définit les valeurs du formulaire
+
+    component.submit(); // Appelle la méthode 'submit'
+
+    expect(sessionLoginSpy).toHaveBeenCalledWith(sessionInformation); // Vérifie si la méthode 'logIn' a été appelée avec les bonnes informations de session
   });
 });
