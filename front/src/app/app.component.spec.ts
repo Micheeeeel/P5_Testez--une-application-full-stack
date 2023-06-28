4ca9f5b7220e9099849247c70bd6e40b54fea8af
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-06-28 10:49:42 +0200

    My first unit test on app.component

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -5,25 +5,36 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
-
+import { AuthService } from './features/auth/services/auth.service';
+import { SessionService } from './services/session.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let authService: AuthService;
+  let sessionsService: SessionService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientModule,
-        MatToolbarModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientModule, MatToolbarModule],
+      declarations: [AppComponent],
+      providers: [AuthService, SessionService],
     }).compileComponents();
+
+    authService = TestBed.inject(AuthService);
+    sessionsService = TestBed.inject(SessionService);
+
+    const fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should call logout', () => {
+    const spy = jest.spyOn(sessionsService, 'logOut');
+    component.logout();
+    expect(spy).toHaveBeenCalled();
   });
 });
