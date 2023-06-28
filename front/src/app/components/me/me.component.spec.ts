afbf843900b8102e85ce5bb75ef82927a837a437
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-06-28 12:10:29 +0200

    Complete tests for me.component component

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -6,19 +6,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
+import { expect } from '@jest/globals';
 
 import { MeComponent } from './me.component';
+import { User } from 'src/app/interfaces/user.interface';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let userService: UserService;
 
   const mockSessionService = {
     sessionInformation: {
       admin: true,
-      id: 1
-    }
-  }
+      id: 1,
+    },
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
@@ -28,11 +33,10 @@ describe('MeComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
       ],
       providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
@@ -40,6 +44,41 @@ describe('MeComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Check that component is created
+
+    expect(component.user).toBeUndefined(); // Check that user is undefined
+  });
+
+  it('should call back method', () => {
+    const spyBack = jest.spyOn(window.history, 'back'); // Use the injected Router
+
+    component.back();
+
+    expect(spyBack).toHaveBeenCalled(); // Check that window.history back method has been called
+  });
+
+  it('should initialize user property', () => {
+    // create a false user object
+    const mockUser: User = {
+      id: 1,
+      email: 'toto@gmail.toto',
+      password: 'toto',
+      admin: true,
+      firstName: 'toto',
+      lastName: 'toto',
+      createdAt: new Date(),
+    };
+
+    // create a spy on the userService getById method
+    const userService = TestBed.inject(UserService);
+    const getByIdSpy = jest
+      .spyOn(userService, 'getById')
+      .mockReturnValue(of(mockUser)); // mock the return value of the getById method
+
+    // call the ngOnInit method
+    component.ngOnInit();
+
+    // check that the user property is equal to the mockUser object
+    expect(component.user).toEqual(mockUser);
   });
 });
