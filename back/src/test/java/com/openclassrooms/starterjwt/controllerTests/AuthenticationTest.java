427f73751c73c16c12b5fe44a4be7360d33b9d6f
Micheeeeel <michelt.thomas@gmail.com>
2023-07-17 11:19:34 +0200

    add integration tests for authentication and session controllers

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllerTests/AuthenticationTest.java
@@ -0,0 +1,121 @@
+package com.openclassrooms.starterjwt.controllerTests;
+
+import com.jayway.jsonpath.JsonPath;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AuthenticationTest {
+
+        @Autowired
+        MockMvc mockMvc;
+
+        // Test qui vérifie si l'utilisateur admin peut se connecter
+        @Test
+        public void itShldLoginAdmin() throws Exception {
+                String email = "yoga@studio.com";
+                String password = "test!1234";
+                String requestBody = "{\"email\": \"" + email + "\", \"password\": \"" + password + "\"}";
+
+                mockMvc.perform(post("/api/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                                .andExpect(status().isOk());
+        }
+
+        // Test qui vérifie qu'un utilisateur non enregistré ne peut pas se connecter
+        @Test
+        public void itShldNotLoginUnregisteredUser() throws Exception {
+                String email = "stillNotRegisterUser@gmail.com";
+                String password = "wrongPassword";
+                String requestBody = "{\"email\": \"" + email + "\", \"password\": \"" + password + "\"}";
+
+                mockMvc.perform(post("/api/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                                .andExpect(status().isUnauthorized());
+        }
+
+        // Test qui vérifie qu'un nouvelle utilisateur peut s'enregistrer
+        @Test
+        public void itShldRegisterNewUser() throws Exception {
+                String email = "toto@gmail.com";
+                String password = "toto!12345";
+                String lastName = "toto";
+                String firstName = "toto";
+                String requestBody = "{" +
+                                "\"email\": \"" + email + "\"," +
+                                "\"password\": \"" + password + "\"," +
+                                "\"firstName\": \"" + firstName + "\"," +
+                                "\"lastName\": \"" + lastName + "\"" +
+                                "}";
+
+                mockMvc.perform(post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("message", is("User registered successfully!")));
+        }
+
+        // Test qui vérifie qu'un utilisateur déjà présent dans la base de données ne
+        // peut pas s'enregistrer
+        @Test
+        public void itShldNotRegisterExistingUser() throws Exception {
+                String email = "toto@gmail.com";
+                String password = "toto!12345";
+                String lastName = "toto";
+                String firstName = "toto";
+                String requestBody = "{" +
+                                "\"email\": \"" + email + "\"," +
+                                "\"password\": \"" + password + "\"," +
+                                "\"firstName\": \"" + firstName + "\"," +
+                                "\"lastName\": \"" + lastName + "\"" +
+                                "}";
+
+                mockMvc.perform(post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                                .andExpect(status().isBadRequest());
+        }
+
+        @AfterAll
+        public void cleanup() throws Exception {
+                // delete the user created in the register method
+                String email = "toto@gmail.com";
+                String password = "toto!12345";
+                // login to get the token
+                String request = "{\"email\": \"" + email + "\", \"password\": \"" + password + "\"}";
+                MvcResult result = mockMvc.perform(post("/api/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request))
+                                .andReturn();
+
+                String content = result.getResponse().getContentAsString();
+                String token = JsonPath.read(content, "$.token");
+                int id = JsonPath.read(content, "$.id"); // get the id of the user
+                // delete the user
+                mockMvc.perform(delete("/api/user/" + id).header("Authorization", "Bearer " + token))
+                                .andExpect(status().isOk());
+        }
+
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllerTests/SessionTests.java
@@ -0,0 +1,207 @@
+package com.openclassrooms.starterjwt.controllerTests;
+
+import com.jayway.jsonpath.JsonPath;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SessionTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    private String token;
+
+    @BeforeAll
+    public void getToken() throws Exception {
+        // Arrange -- créer un compte admin
+        String email = "yoga@studio.com";
+        String password = "test!1234";
+        String requestBody = "{\"email\": \"" + email + "\", \"password\": \"" + password + "\"}";
+
+        // récupérer le token
+        MvcResult result = mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andReturn();
+
+        token = "Bearer " + JsonPath.read(result.getResponse().getContentAsString(), "$.token");
+    }
+
+    @BeforeEach
+    // création d'une session
+    public void createSession() throws Exception {
+        // Arrange -- create a session
+        String name = "First Session";
+        long teacherID = 1L;
+        String description = "description example";
+        String date = "2023-01-01";
+
+        String body = "{" +
+                "\"name\": \"" + name + "\"," +
+                "\"teacher_id\":" + teacherID + "," +
+                "\"description\":\"" + description + "\"," +
+                "\"date\":\"" + date + "\"}";
+
+        // Act -- simulate a post request to the route /api/session/
+        ResultActions result = mockMvc.perform(post("/api/session/")
+                .contentType(MediaType.APPLICATION_JSON).content(body)
+                .header("Authorization", token));
+        // Assert -- assert that the status is 200 and that the returned name is the
+        // same
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("name", is(name)));
+    }
+
+    @AfterEach
+    // suppréssion de la session
+    public void deleteSession() throws Exception {
+        // Arrange -- identifier l'id de la session à supprimer
+        ResultActions resultGet = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        String content = resultGet.andReturn().getResponse().getContentAsString();
+        int id = JsonPath.read(content, "$[0].id");
+
+        // Act -- simulate a delete request to the route /api/session/id
+        ResultActions result = mockMvc.perform(delete("/api/session/" + id).header("Authorization", token));
+        // Assert -- assert that the status is 200
+        result.andExpect(status().isOk());
+    }
+
+    // Test de la route /api/session/
+    @Test
+    public void itShouldGetAllSessions() throws Exception {
+        // Act -- simulate a get request to the route /api/session/
+        ResultActions result = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        // Assert -- assert that the status is 200
+        result.andExpect(status().isOk());
+
+    }
+
+    // tester la route /api/session/{id}
+    @Test
+    public void itShldGetOneSession() throws Exception {
+        // Arrange -- identifier l'id de la session à supprimer
+        ResultActions resultGet = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        String content = resultGet.andReturn().getResponse().getContentAsString();
+        int id = JsonPath.read(content, "$[0].id");
+
+        // Act -- simulate a get request to the route /api/session/id
+        ResultActions result = mockMvc.perform(get("/api/session/" + id).header("Authorization", token));
+        // Assert -- assert that the status is 200
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void itShldthrowAnExceptionWhenGettingInvalidUrl() throws Exception {
+        // Act -- simulate a get request to the route /api/session/Invalid
+        ResultActions result = mockMvc.perform(get("/api/session/Invalid").header("Authorization", token));
+        // Assert -- simulate a get request to the route /api/session/Invalid
+        result.andExpect(status().isBadRequest());
+    }
+
+    // tester la modification d'une session
+    @Test
+    public void itShldUpdateSession() throws Exception {
+        // Arrange -- create a session
+        String name = "Updated session example";
+        long teacherID = 1L;
+        String description = "description example";
+        String date = "2023-01-01";
+
+        String body = "{" +
+                "\"name\": \"" + name + "\"," +
+                "\"teacher_id\":" + teacherID + "," +
+                "\"description\":\"" + description + "\"," +
+                "\"date\":\"" + date + "\"}";
+
+        // Arrange -- identifier l'id de la session à supprimer
+        ResultActions resultGet = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        String content = resultGet.andReturn().getResponse().getContentAsString();
+        int id = JsonPath.read(content, "$[0].id");
+
+        // Act -- simulate a post request to the route /api/session/
+        ResultActions result = mockMvc.perform(put("/api/session/" + id) // 1 because we already created a session with
+                                                                         // id
+                // 1 into previous test
+                .contentType(MediaType.APPLICATION_JSON).content(body)
+                .header("Authorization", token));
+        // Assert -- assert that the status is 200 and that the returned name is the new
+        // one
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("name", is(name)));
+    }
+
+    // tester la route /api/session/{id}/participate
+    @Test
+    public void itShldParticipate() throws Exception {
+        // Arrange -- identifier l'id de la session à supprimer
+        ResultActions resultGet = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        String content = resultGet.andReturn().getResponse().getContentAsString();
+        int id = JsonPath.read(content, "$[0].id");
+
+        // Act -- simulate a post request to the route /api/session/id/participate
+        ResultActions result = mockMvc
+                .perform(post("/api/session/" + id + "/participate/1").header("Authorization", token));
+        // Assert -- assert that the status is 200
+        result.andExpect(status().isOk());
+    }
+
+    // tester la désinscription d'une session
+    @Test
+    public void itShldNoLongerParticipate() throws Exception {
+
+        itShldParticipate();
+
+        // Arrange -- identifier l'id de la session à supprimer
+        ResultActions resultGet = mockMvc.perform(get("/api/session/").header("Authorization", token));
+        String content = resultGet.andReturn().getResponse().getContentAsString();
+        int id = JsonPath.read(content, "$[0].id");
+
+        // Act -- simulate a delete request to the route /api/session/id/participate/1
+        ResultActions result = mockMvc
+                .perform(delete("/api/session/" + id + "/participate/1").header("Authorization", token));
+        // Assert -- assert that the status is 200
+        result.andExpect(status().isOk());
+    }
+
+    // tester la suppression d'un compte n'existant pas
+    @Test
+    public void itShldThrowAnExceptionWhenDeletingInvalidSession() throws Exception {
+        // Act -- simulate a delete request to the route /api/session/1
+        ResultActions result = mockMvc.perform(delete("/api/session/0").header("Authorization", token));
+        // Assert -- assert that the status is 404
+        result.andExpect(status().isNotFound());
+    }
+
+    // tester la suppression d'un compte avec un id invalide
+    @Test
+    public void itShldThrowAnExceptionWhenDeletingSessionWithInvalidId() throws Exception {
+        // Act -- simulate a delete request to the route /api/session/1
+        ResultActions result = mockMvc.perform(delete("/api/session/invalid").header("Authorization", token));
+        // Assert -- assert that the status is 400
+        result.andExpect(status().isBadRequest());
+    }
+
+}
